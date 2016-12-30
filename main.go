6734b9d2a1a41e9a37c1a17bcb513e11a4772a10
Randy Fay <randy@randyfay.com>
2016-12-29 19:32:29 -0700

    Simply presentation and allow to select the policy

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
+// Simple tool to walk all vault accessors and display those associated
+// with a particular policy, 'root' by default.
+// vault_token_lister -targetVaultAddr=https://example.com:8200 -rootToken=someroot-7644-a9aa
+
 package main
 
 import (
 	"log"
 	vaultAPI "github.com/hashicorp/vault/api"
 	"flag"
-	"bufio"
 	"os"
 	"fmt"
 )
 
-
 func main() {
 
-	var targetVaultAddr, vaultRootToken string
+	var targetVaultAddr, vaultRootToken, policy string
 	flag.StringVar(&targetVaultAddr, "targetVaultAddr", "", "vault_addr for the target vault, like https://example2.com:8200")
 	flag.StringVar(&vaultRootToken, "rootToken", "", "Token for targetVaultAddr - must have root privs")
+	flag.StringVar(&policy, "policy", "root", "Name of policy we're looking for, by default 'root'")
 	flag.Parse()
 
 	vClient := getVaultClient(vaultRootToken, targetVaultAddr)
-
-	//fmt.Printf("Getting token accessors from %s\n", targetVaultAddr)
-	//confirm("Are you sure you want to continue? [y/n]")
 
 	self, err := vClient.Auth().Token().LookupSelf()
 	if err != nil {
@@ -37,49 +37,36 @@ func main() {
 		//fmt.Printf("got %T\n", accessors)
 		for _, accessor := range accessors {
 			//fmt.Println(i, accessor)
-			details, err := inspectAccessor(vClient, accessor.(string))
+			details, err := vClient.Auth().Token().LookupAccessor(accessor.(string))
 			if (err != nil) {
 				panic(err)
 			}
 			policies := details.Data["policies"]
+			displayName := details.Data["display_name"].(string)
 			switch typedPolicies := policies.(type) {
 			case []interface{}:
-				for _, policy := range typedPolicies {
+				for _, p := range typedPolicies {
 					//fmt.Printf(" %s ", policy.(string))
-					if policy.(string) == "root" {
+					if p.(string) == policy {
+						output := fmt.Sprintf("%s accessor displayName=%s: revoke if you will with 'vault token-revoke -accessor %s'\n", policy, displayName, accessor)
 						if selfAccessor == accessor {
-							fmt.Printf("Root accessor THIS IS YOU, revoke if you will with 'vault token-revoke -accessor %s'\n", accessor)
-						} else {
-							fmt.Printf("Root accessor, revoke with 'vault token-revoke -accessor %s'\n", accessor)
+							output = "THIS IS YOU, " + output
 						}
+						fmt.Println(output)
 
 					}
 				}
 			}
 
-			//fmt.Printf("Accessor: %d %s: \n\t%v\t%v\n", i, accessor, policies, details)
+			//fmt.Printf("Accessor: %s: \n\t%v\t%v\n", accessor, policies, details)
 		}
 	default:
 		fmt.Printf("I don't know how to handle %T\n", result.Data["keys"])
 		os.Exit(2)
 	}
 
-	//targetVault.Auth().Token()
-	// Get the list of accessors (through direct https query I guess)
-	// Walk the list of accessors and gather/present info based on what they are.
 }
 
-
-// Simple confirmation prompt. return true on 'y'
-func confirm(prompt string) bool {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(prompt + ":")
-	response, _ := reader.ReadString('\n')
-	if (response != "y\n") {
-		return false
-	}
-	return true
-}
 
 
 // Get vault object using token + vault_addr
@@ -101,6 +88,7 @@ func getVaultClient(token string, vaultAddr string) vaultAPI.Client {
 	return *vClient
 }
 
+// Use API to get list of all token accessors
 func listAccessors(client vaultAPI.Client) (*vaultAPI.Secret, error) {
 	request := client.NewRequest("LIST", "/v1/auth/token/accessors")
 
@@ -110,22 +98,5 @@ func listAccessors(client vaultAPI.Client) (*vaultAPI.Secret, error) {
 	}
 	defer resp.Body.Close()
 
-	return vaultAPI.ParseSecret(resp.Body)
-}
-
-func inspectAccessor(client vaultAPI.Client, accessor string)  (*vaultAPI.Secret, error) {
-	request := client.NewRequest("POST", "/v1/auth/token/lookup-accessor")
-
-	if err := request.SetJSONBody(map[string]interface{}{
-		"accessor": accessor,
-	}); err != nil {
-		return nil, err
-	}
-	resp, err := client.RawRequest(request)
-	if (err != nil) {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
 	return vaultAPI.ParseSecret(resp.Body)
 }
